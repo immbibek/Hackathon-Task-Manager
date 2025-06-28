32f39c90e187fc409404b2e91a09ad797f0b0324
Bibek <137331749+immbibek@users.noreply.github.com>
2025-06-28 21:10:04 +0530

    Initial setup for a Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className=" bg-amber-50">
+      <h1 className=" text-amber-200">Hello Wolrd</h1>
+    </div>
+  );
+};
+
+export default App;
