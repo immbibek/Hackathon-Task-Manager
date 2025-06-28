e84dc5bee668c6b3b1329a2e9680518df13aeb7f
Bibek <137331749+immbibek@users.noreply.github.com>
2025-06-28 14:27:24 +0530

    middleware for JWT varification

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from "dotenv";
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = 5000;
